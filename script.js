96364d8c9464884369721326e822ceded0d827f2
spaaxrks <akashj.dev@gmail.com>
2025-06-10 13:38:27 +0530

    improved chessboard design made responsive and improvised logic

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const root = document.querySelector(".root");
 
-const chessboard = ()=>{
+//inserting Heading
+document.body.insertAdjacentHTML("afterBegin",`<div class="headingdiv"></div>`);
+const headingdiv = document.querySelector(".headingdiv");
+
+const playchess=document.createElement("h1");
+playchess.innerHTML="Play<span>Chess</span>JS";
+headingdiv.append(playchess);
+
+
+//creating chessboard logic
+const chessboard = ()=>{              
+
     for(let i=1;i<=8;i++){
         if(i%2!==0){
             for(let j=1;j<=8;j++){
@@ -10,6 +21,7 @@ const chessboard = ()=>{
                     blackbox.classList.add("blackbox");
 
                     blackbox.id=`${i}${j}`;
+                    blackbox.onclick= selectpiece;              //setting up event listener in HTML tag itself and calling selectpiece() function though through this method the onclick wont be directly shown inside the html but its still there.
                     root.append(blackbox);
                 }
                 else{
@@ -17,6 +29,7 @@ const chessboard = ()=>{
                     const whitebox = document.createElement("div");
                     whitebox.classList.add("whitebox");
                     whitebox.id=`${i}${j}`;
+                    whitebox.onclick= selectpiece;
                     root.append(whitebox);
                 }
 
@@ -30,6 +43,7 @@ const chessboard = ()=>{
                     const whitebox = document.createElement("div");
                     whitebox.classList.add("whitebox");
                     whitebox.id=`${i}${j}`;
+                    whitebox.onclick= selectpiece;
                     root.append(whitebox);
                 }
                 else{
@@ -37,34 +51,45 @@ const chessboard = ()=>{
                     const blackbox = document.createElement("div");
                     blackbox.classList.add("blackbox");
                     blackbox.id=`${i}${j}`;
+                    blackbox.onclick= selectpiece;
                     root.append(blackbox);
                 }
 
             }
         }
     }
+
 }
 chessboard();
-// for(let i=1;i)
 
+//main object for tracking positions.
 const pieces ={
-    "11":"./pieces/black-rook.png","12":"./pieces/black-knight.png","13":"./pieces/black-bishop.png","14":"./pieces/black-queen.png","15":"./pieces/black-king.png","16":"./pieces/black-bishop.png","17":"./pieces/black-knight.png","18":"./pieces/black-rook.png",
-    "21":"./pieces/black-pawn.png","22":"./pieces/black-pawn.png","23":"./pieces/black-pawn.png","24":"./pieces/black-pawn.png","25":"./pieces/black-pawn.png","26":"./pieces/black-pawn.png","27":"./pieces/black-pawn.png","28":"./pieces/black-pawn.png",
-    "71":"./pieces/white-pawn.png","72":"./pieces/white-pawn.png","73":"./pieces/white-pawn.png","74":"./pieces/white-pawn.png","75":"./pieces/white-pawn.png","76":"./pieces/white-pawn.png","77":"./pieces/white-pawn.png","78":"./pieces/white-pawn.png",
-    "81":"./pieces/white-rook.png","82":"./pieces/white-knight.png","83":"./pieces/white-bishop.png","84":"./pieces/white-queen.png","85":"./pieces/white-king.png","86":"./pieces/white-bishop.png","87":"./pieces/white-knight.png","88":"./pieces/white-rook.png",
+    "b-rook1":"11","b-knight1":"12","b-bishop1":"13","b-queen":"14","b-king":"15","b-bishop2":"16","b-knight2":"17","b-rook2":"18",
+    "b-pawn1":"21","b-pawn2":"22","b-pawn3":"23","b-pawn4":"24","b-pawn5":"25","b-pawn6":"26","b-pawn7":"27","b-pawn8":"28",
+    "w-pawn1":"71","w-pawn2":"72","w-pawn3":"73","w-pawn4":"74","w-pawn5":"75","w-pawn6":"76","w-pawn7":"77","w-pawn8":"78",
+    "w-rook1":"81","w-knight1":"82","w-bishop1":"83","w-queen":"84","w-king":"85","w-bishop2":"86","w-knight2":"87","w-rook2":"88",
 };
 
+const images =  {
+    "b-rook1":"./pieces/black-rook.png","b-knight1":"./pieces/black-knight.png","b-bishop1":"./pieces/black-bishop.png","b-queen":"./pieces/black-queen.png","b-king":"./pieces/black-king.png","b-bishop2":"./pieces/black-bishop.png","b-knight2":"./pieces/black-knight.png","b-rook2":"./pieces/black-rook.png",
+    "b-pawn1":"./pieces/black-pawn.png","b-pawn2":"./pieces/black-pawn.png","b-pawn3":"./pieces/black-pawn.png","b-pawn4":"./pieces/black-pawn.png","b-pawn5":"./pieces/black-pawn.png","b-pawn6":"./pieces/black-pawn.png","b-pawn7":"./pieces/black-pawn.png","b-pawn8":"./pieces/black-pawn.png",
+    "w-rook1":"./pieces/white-rook.png","w-knight1":"./pieces/white-knight.png","w-bishop1":"./pieces/white-bishop.png","w-queen":"./pieces/white-queen.png","w-king":"./pieces/white-king.png","w-bishop2":"./pieces/white-bishop.png","w-knight2":"./pieces/white-knight.png","w-rook2":"./pieces/white-rook.png",
+    "w-pawn1":"./pieces/white-pawn.png","w-pawn2":"./pieces/white-pawn.png","w-pawn3":"./pieces/white-pawn.png","w-pawn4":"./pieces/white-pawn.png","w-pawn5":"./pieces/white-pawn.png","w-pawn6":"./pieces/white-pawn.png","w-pawn7":"./pieces/white-pawn.png","w-pawn8":"./pieces/white-pawn.png",
+}
 
+
+//logic for putting pieces in chessboard
 const putpieces = (pieces)=>{
 
-    for(let id in pieces){
+    for(let piecename in pieces){
         
-        const box = document.getElementById(id);
+        const box = document.getElementById(pieces[piecename]);
         const img =document.createElement("img");
-        img.src=pieces[id];
-        img.width=50;
-        img.height=50;
+        img.src=images[piecename];
         box.append(img);
+        
     }
 }
 putpieces(pieces);
+
+
